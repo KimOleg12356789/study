183b24603d0a14b1019362fc8aa63327cdf8cb24
Kim Oleg <88618558+KimOleg12356789@users.noreply.github.com>
2023-03-10 16:23:18 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+$( document ).ready(function() {
+
+    $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 769,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+        }
+    ]
+});
+
+
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.mt-mobile');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('nav-open');
+    body.classList.toggle('body-open');
+    burger.classList.toggle('burger-open');
+    
+});
+
+(function() {
+    
+    function controls() {
+      $(this).addClass('selected').siblings('button').removeClass('selected');
+      $('.slideDiv').animate({
+        top: -$('.container').height() * $(this).index()
+      }, 450);
+    }
+    
+    /***Event Listeners***/
+    const runCode = () => {
+      const button = document.querySelectorAll('.button');
+      if ( button ) {
+        for ( var i = 0; i < button.length; i++ ) {
+          button[i].addEventListener('click', controls, false);
+        }
+      }
+    }
+    
+    runCode();
+    
+  })();
+
+
+
+
+
+
+});
